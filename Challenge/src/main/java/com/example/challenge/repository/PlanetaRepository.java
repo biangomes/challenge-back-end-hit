033022278206a80600ae9ce84944910594ef8863
biangomes <biangomes0707@gmail.com>
2022-11-22 17:08:52 -0300

    Properties e PlanetaRepository arrumados

--- Challenge/src/main/java/com/example/challenge/repository/PlanetaRepository.java
+++ Challenge/src/main/java/com/example/challenge/repository/PlanetaRepository.java
@@ -1,4 +1,14 @@
 package com.example.challenge.repository;
 
-public class PlanetaRepository {
+import com.example.challenge.model.Planeta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PlanetaRepository extends JpaRepository<Planeta, Long> {
+
+    List<Planeta> findAll();
+
+    Planeta findByNomePlaneta(String nome);
 }
