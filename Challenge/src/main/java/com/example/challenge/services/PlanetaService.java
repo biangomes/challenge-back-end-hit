8718b71948fd8cad2b73f2b245d29f2d76bf4c33
biangomes <biangomes0707@gmail.com>
2022-11-23 17:02:46 -0500

    Planeta service mapeado como service

--- Challenge/src/main/java/com/example/challenge/services/PlanetaService.java
+++ Challenge/src/main/java/com/example/challenge/services/PlanetaService.java
@@ -1,4 +1,18 @@
 package com.example.challenge.services;
 
-public class PlanetaService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.challenge.repository.PlanetaRepository;
+
+@Service
+public class PlanetaService<Planeta> {
+
+    private final PlanetaRepository planetaRepository;
+
+    // Utilizando injeção de dependência
+    @Autowired
+    public PlanetaService(PlanetaRepository planetaRepository) {
+        this.planetaRepository = planetaRepository;
+    }
 }
